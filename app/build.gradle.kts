26549b6d67a09107fab8453985b3a077f9272ddb
Admin <shade641114@gmail.com>
2024-04-30 18:57:25 +0300

    Exercise 1-3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,14 @@
 plugins {
     id("java")
+    application
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass = "hexlet.code.App"
+}
 
 repositories {
     mavenCentral()
@@ -12,6 +17,11 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
 }
 
 tasks.test {
---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,16 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+
+public class Cli {
+    public static void scannerName() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("May I have your name? ");
+        String userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
+
+        scanner.close();
+    }
+}
